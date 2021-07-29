026908aab6aa6c5d3c27b7e4ed59adf3b66f6c92
drathore598 <drathore598@gmail.com>
2021-07-29 21:44:57 +0530

    Added expense model serializer and a negative test without all the required fields. And Refactored behavior for generic views.

--- restapi/tests.py
+++ restapi/tests.py
@@ -22,7 +22,7 @@ class TestModels(TestCase):
 class TestViews(TestCase):
     def test_expense_create(self):
         payload = {
-            "amount": 50,
+            "amount": 50.0,
             "merchant": "AT&T",
             "description": "cell phone subscription",
             "category": "utilities",
@@ -36,7 +36,7 @@ class TestViews(TestCase):
 
         json_res = res.json()
 
-        self.assertEqual(str(payload["amount"]), json_res["amount"])
+        self.assertEqual(payload["amount"], json_res["amount"])
         self.assertEqual(payload["merchant"], json_res["merchant"])
         self.assertEqual(payload["description"], json_res["description"])
         self.assertEqual(payload["category"], json_res["category"])
@@ -53,3 +53,45 @@ class TestViews(TestCase):
 
         expenses = models.Expense.objects.all()
         self.assertEqual(len(expenses), len(json_res))
+
+    def test_expense_create_required_fields_missing(self):
+        payload = {
+            "merchant": "AT&T",
+            "description": "cell phone subscription",
+            "category": "utilities",
+        }
+
+        res = self.client.post(
+            reverse("restapi:expense-list-create"), payload, format="json"
+        )
+
+        self.assertEqual(400, res.status_code)
+
+    def test_expense_retrieve(self):
+        expense = models.Expense.objects.create(
+            amount=300, merchant="George", description="loan", category="transfer"
+        )
+        res = self.client.get(
+            reverse("restapi:expense-retrieve-delete", args=[expense.id]), format="json"
+        )
+
+        self.assertEqual(200, res.status_code)
+
+        json_res = res.json()
+
+        self.assertEqual(expense.id, json_res["id"])
+        self.assertEqual(expense.amount, json_res["amount"])
+        self.assertEqual(expense.merchant, json_res["merchant"])
+        self.assertEqual(expense.description, json_res["description"])
+        self.assertEqual(expense.category, json_res["category"])
+
+    def test_expense_delete(self):
+        expense = models.Expense.objects.create(
+            amount=400, merchant="John", description="loan", category="transfer"
+        )
+        res = self.client.delete(
+            reverse("restapi:expense-retrieve-delete", args=[expense.id]), format="json"
+        )
+
+        self.assertEqual(204, res.status_code)
+        self.assertFalse(models.Expense.objects.filter(pk=expense.id).exists())
--- restapi/views.py
+++ restapi/views.py
@@ -1,26 +1,15 @@
 from django.shortcuts import render
-from rest_framework.views import APIView
 from rest_framework.response import Response
-from restapi import models
+from restapi import models, serializers
 from django.forms.models import model_to_dict
+from rest_framework.generics import RetrieveDestroyAPIView, ListCreateAPIView
 
 # Create your views here.
-class ExpenseListCreate(APIView):
-    def get(self, request):
-        expenses = models.Expense.objects.all()
+class ExpenseListCreate(ListCreateAPIView):
+    serializer_class = serializers.Expense
+    queryset = models.Expense.objects.all()
 
-        all_expenses = [model_to_dict(expenses) for expense in expenses]
 
-        return Response(all_expenses, status=200)
-
-    def post(self, request):
-        amount = request.data["amount"]
-        merchant = request.data["merchant"]
-        description = request.data["description"]
-        category = request.data["category"]
-
-        expense = models.Expense.objects.create(
-            amount=amount, merchant=merchant, description=description, category=category
-        )
-
-        return Response(model_to_dict(expense), status=201)
+class ExpenseRetrieveDelete(RetrieveDestroyAPIView):
+    serializer_class = serializers.Expense
+    queryset = models.Expense.objects.all()
